ead02a9ffaf73d84b7cfa33c7d9764d1241b633d
KlartNET <dev1234@klartnet.ml>
2022-01-23 10:26:48 +0900

    Update index.user.js

--- 3DPChip_Sponsor_AdBlocker/index.user.js
+++ 3DPChip_Sponsor_AdBlocker/index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         3DPChip Sponsor AdBlocker
 // @author       KlartNET
-// @version      0.0.1
+// @version      0.0.2
 // @description  3DPChip Sponsor AdBlocker
 // @updateURL    https://github.com/JadeMin/UserScripts/raw/main/3DPChip_Sponsor_AdBlocker/index.user.js
 // @match        *://www.3dpchip.com/*
