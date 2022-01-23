f214ad7a018bf223f0d5e90e9ed2def64f22fb37
KlartNET <dev1234@klartnet.ml>
2022-01-23 10:18:26 +0900

    Create index.user.js

---
+++ 3DPChip_Sponsor_AdBlocker/index.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         3DPChip Sponsor AdBlocker
+// @author       KlartNET
+// @version      0.0.1
+// @description  3DPChip Sponsor AdBlocker
+// @updateURL    null
+// @match        *://www.3dpchip.com/*
+// @run-at       document-start
+// ==/UserScript==
+
+(function(_this) {
+	'use strict';
+	
+	location.pathname == "/3dp/ad/index.html" && window.close();
+})(this);
