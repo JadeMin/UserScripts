62595167a91954761ada461d0a5b17b902a071f5
KlartNET <dev1234@klartnet.ml>
2022-01-26 14:39:13 +0900

    3DPChip_

--- 3DPChip_Sponsor_AdBlock/index.user.js
+++ 3DPChip_Sponsor_AdBlock/index.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name         3DPChip Sponsor AdBlock
 // @author       KlartNET
-// @version      0.0.7
+// @version      0.0.8
 // @description  3DPChip Sponsor AdBlock
 // @updateURL    https://github.com/JadeMin/UserScripts/raw/main/3DPChip_Sponsor_AdBlock/index.user.js
 // @downloadURL  https://github.com/JadeMin/UserScripts/raw/main/3DPChip_Sponsor_AdBlock/index.user.js
 // @match        *://www.3dpchip.com/*
+// @run-at       document-start
 // ==/UserScript==
 
 (function(_this) {
 	'use strict';
-	
+
 	location.pathname == "/3dp/ad/index.html" && window.close();
 })(this);
