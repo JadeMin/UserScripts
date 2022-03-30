9f7df252a315e418efbccbaa8ed81d3efd36b18e
KlartNET <dev1234@klartnet.ml>
2022-03-31 00:07:25 +0900

    Update index.user.js

--- 3DPChip_Sponsor_AdBlock/index.user.js
+++ 3DPChip_Sponsor_AdBlock/index.user.js
@@ -6,7 +6,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=www.3dpchip.com
 // @updateURL    https://github.com/JadeMin/UserScripts/raw/main/3DPChip_Sponsor_AdBlock/index.user.js
 // @downloadURL  https://github.com/JadeMin/UserScripts/raw/main/3DPChip_Sponsor_AdBlock/index.user.js
-// @match        *://www.3dpchip.com/*
+// @match        *://*.3dpchip.com/*
 // @run-at       document-start
 // ==/UserScript==
 
